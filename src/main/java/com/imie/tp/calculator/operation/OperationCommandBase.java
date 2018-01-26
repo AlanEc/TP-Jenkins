78f71bd21ccfdafcceda864768121daa4bbe0325
Ecalle <alan.ecalle@laposte.net>
2018-01-26 12:36:05 +0100

    correction erreurs

--- src/main/java/com/imie/tp/calculator/operation/OperationCommandBase.java
+++ src/main/java/com/imie/tp/calculator/operation/OperationCommandBase.java
@@ -6,8 +6,6 @@ package com.imie.tp.calculator.operation;
  */
 public abstract class OperationCommandBase implements OperationCommand {
 
-  public float currentValue;
-    
   public OperationCommandBase(float baseValue)
   {
             this.currentValue = baseValue;
