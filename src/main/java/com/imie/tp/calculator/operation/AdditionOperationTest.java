b4265d7d8556a9029a8f5072ef4a3cb847e748f1
Ecalle <alan.ecalle@laposte.net>
2018-01-26 15:13:47 +0100

    Tests

---
+++ src/main/java/com/imie/tp/calculator/operation/AdditionOperationTest.java
@@ -0,0 +1,14 @@
+package com.imie.tp.calculator.operation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdditionOperationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
